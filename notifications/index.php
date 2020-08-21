40decf0006cd9507bac9641868332bdad0c5c157
Lucas Pallares <lucas@kubeymachine.com>
2020-08-21 11:44:00 -0300

    Update

--- notifications/index.php
+++ notifications/index.php
@@ -93,10 +93,10 @@ try {
     //Server settings
     $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
     $mail->isSMTP();                                            // Send using SMTP
-    $mail->Host       = 'smtp-relay.sendinblue.com';                    // Set the SMTP server to send through
+    $mail->Host       = '';                    // Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-    $mail->Username   = 'support@kubeymachine.com';                     // SMTP username
-    $mail->Password   = '5pDhJT8RbkSLgEPX';                               // SMTP password
+    $mail->Username   = '';                     // SMTP username
+    $mail->Password   = '';                               // SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
     $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
